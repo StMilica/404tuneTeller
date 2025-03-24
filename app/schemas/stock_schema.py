9bd65aea8f072ac9ee67290d32653a315240f860
Milica <milica93.stamenkovic@gmail.com>
2025-03-24 22:48:18 +0100

    Cleaning

--- app/schemas/stock_schema.py
+++ app/schemas/stock_schema.py
@@ -6,12 +6,11 @@ from app import db
 class StockSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Stock
-        load_instance = True  # allows .load() to return Stock objects
+        load_instance = True
         sqla_session = db.session
         include_relationships = False
         include_fk = False
 
-    # Override default field settings for custom validation
     name = fields.Str(required=True, validate=validate.Length(min=1))
     symbol = fields.Str(required=True, validate=validate.Length(min=1, max=10))
     founded = fields.Date(required=True, format='%Y-%m-%d')
